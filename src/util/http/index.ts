a5bfedeed3d2b38d431591b7be12a48d3e6b035a
Vladimir Leonov <fatur_muhammad@bk.ru>
2026-07-08 18:16:20 +0700

    rollback

--- src/util/http/index.ts
+++ src/util/http/index.ts
@@ -20,33 +20,10 @@ interface RetryRequestConfig extends AxiosRequestConfig {
    BACKEND SELECTOR
 ========================= */
 
-const backendUrls = [
-    import.meta.env.VITE_BACKEND_URL_PROD,
-    import.meta.env.VITE_BACKEND_URL_PROD_2,
-].filter(Boolean);
-
-let activeBackendIndex = 0;
-
 const getBackendUrl = () => {
-    if (import.meta.env.VITE_ENVIRONMENT === "development")
-    {
-        return import.meta.env.VITE_BACKEND_URL_DEV;
-    }
-
-    return backendUrls[activeBackendIndex];
-};
-
-const switchBackend = () => {
-    if (backendUrls.length <= 1)
-        return;
-
-    activeBackendIndex =
-        (activeBackendIndex + 1) % backendUrls.length;
-
-    console.warn(
-        "Switch backend:",
-        backendUrls[activeBackendIndex]
-    );
+    return import.meta.env.VITE_ENVIRONMENT === "development"
+        ? import.meta.env.VITE_BACKEND_URL_DEV
+        : import.meta.env.VITE_BACKEND_URL_PROD;
 };
 
 /* =========================
@@ -138,10 +115,7 @@ export async function refreshAccessToken()
 
 request.interceptors.request.use(async (config) =>
 {
-    if (!config.baseURL)
-    {
-        config.baseURL = getBackendUrl();
-    }
+    config.baseURL = getBackendUrl();
 
     if (!isBackendRequest(config))
     {
@@ -214,19 +188,7 @@ request.interceptors.response.use(
         console.log("INTERCEPTOR:", status, originalRequest?.url);
 
         // network error (CORS dll)
-        if (!error.response || [502, 503, 504].includes(error.response.status))
-        {
-            if (!originalRequest._retry)
-            {
-                originalRequest._retry = true;
-
-                switchBackend();
-
-                originalRequest.baseURL = getBackendUrl();
-
-                return request(originalRequest);
-            }
-
+        if (!error.response) {
             return Promise.reject(error);
         }
 
